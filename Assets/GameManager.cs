59681a4eae8a9db13918286df8191126bdb474d4
WilliamLarios <williamjlarios@gmail.com>
2024-10-25 15:19:51 -0500

    Fixed switch statement

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -167,6 +167,7 @@ public class GameManager : MonoBehaviour
                 break;
             default:
                 Debug.Log("Tried to switch to invalid mode");
+                break;
         }
     }
     /// <summary>
